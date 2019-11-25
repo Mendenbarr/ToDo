a1e3c7daaefadbebc3d636251c76768598379096
01048750 <01048750@NK-LABW14017.ncc.commnet.edu>
2019-11-25 11:31:31 -0500

    init commit

---
+++ src/GUIComponents/JButtonIcon.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUIComponents;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author 01048750
+ */
+public class JButtonIcon extends JButton {
+
+    public JButtonIcon() {
+        super();
+        this.setBorderPainted(false);
+        this.setBorder(null);
+        this.setMargin(new Insets(0, 0, 0, 0));
+        this.setContentAreaFilled(false);
+    }
+    
+    
+    public void setImage(String image){
+        ImageIcon IconBase = new ImageIcon(image);
+        IconBase.setImage(getScaledImage(IconBase, 12, 12));
+        this.setIcon(IconBase);
+        ImageIcon closeIconRollover = new ImageIcon(getScaledImage(IconBase, 15, 15));
+        this.setRolloverIcon(closeIconRollover);
+        ImageIcon closeIconSelected = new ImageIcon(getScaledImage(IconBase, 15, 15));
+        this.setSelectedIcon(closeIconSelected);
+        ImageIcon closeIconPressed = new ImageIcon(getScaledImage(IconBase, 15, 15));
+        this.setPressedIcon(closeIconPressed);
+        ImageIcon closeIconDisabled = new ImageIcon(getScaledImage(IconBase, 15, 15));
+        this.setDisabledIcon(closeIconDisabled);
+    }
+
+    // Returns an image scaled to width w and height h, in pixels.
+    private Image getScaledImage(ImageIcon srcImg, int w, int h) {
+        BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = resizedImg.createGraphics();
+
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.drawImage(srcImg.getImage(), 0, 0, w, h, null);
+        g2.dispose();
+
+        return resizedImg;
+    }
+
+}
